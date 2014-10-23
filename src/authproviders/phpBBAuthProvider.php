d3b3a2898c3e46b3ac126aa0ed11bb63cfbc2dfa
Niklas Hedlund <nojan1989@gmail.com>
2014-10-23 15:46:43 +0200

    Basic phpBB integration

--- src/authproviders/phpBBAuthProvider.php
+++ src/authproviders/phpBBAuthProvider.php
@@ -3,20 +3,45 @@
 include "base/authProvider.php";
 include_once "authmanager.php";
 
+define('IN_PHPBB', true);
+define('PHPBB_ROOT_PATH', '../../phpBB3/');
+
 class phpBBAuthProvider extends AuthProvider {
 	function __construct(){
+		$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
+		$phpEx = substr(strrchr(__FILE__, '.'), 1);
+		include($phpbb_root_path . 'common.' . $phpEx);
+		include($phpbb_root_path . 'includes/functions_user.php' . $phpEx);
 		
+		// Start session management
+		$user->session_begin();
+		$auth->acl($user->data);
+		$user->setup();
+		
+		$this->user = $user;
 	}
 
 	function GetUser() {
-		return "nojan";
+		if($this->user->data['user_id'] == ANONYMOUS)
+			return null;
+	
+		return $this->user->data['username_clean'];
 	}
 	
 	function GetGroups() {
-		return "admins";
+		if($this->user->data['user_id'] == ANONYMOUS)
+			return array();
+	
+		$groups = group_memberships(false, $this->user->data['user_id'], false);
+		echo $groups;
+		
 	}
 	
 	function GetAuthLevel() {
+		if($this->user->data['user_id'] == ANONYMOUS)
+			return NOT_LOGGED_IN;
+	
+		
 		return ADMIN;
 	}
 }
