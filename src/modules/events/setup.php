631c5720aae28e126c429c6e0cac54c8e3d96c1d
Niklas Hedlund <nojan1989@gmail.com>
2014-11-06 11:06:01 +0100

    Added EndDate to events module

--- src/modules/events/setup.php
+++ src/modules/events/setup.php
@@ -8,11 +8,12 @@ class setup_events extends Installer {
 							eventId int auto_increment primary key,
 							Title varchar(50) not null,
 							Location varchar(50) not null,
-							Date datetime not null,
+							StartDate datetime not null,
+							EndDate datetime not null,
 							Description text
 						);");
 		
-		$this->db->exec("insert into events values (0, 'Test Event', 'Some place more familiar', now(), 'Test event automagically added by the setup script');");
+		$this->db->exec("insert into events values (0, 'Test Event', 'Some place more familiar', now(), date_add(now(), interval 2 hour), 'Test event automagically added by the setup script');");
 		
 		return true;
 	}
