7c3706bd9cf6a8ef8429c8eca237cd4fbe67b17d
Niklas Hedlund <nojan1989@gmail.com>
2014-11-10 16:00:15 +0100

    CRUD implemented for imagegallery

--- src/modules/gallery/module-gallery.php
+++ src/modules/gallery/module-gallery.php
@@ -11,17 +11,41 @@ class gallery extends Module {
 
 	function RegisterRoutes($route){
 		$route->register($this, "|^\/$|", array($this, "listGalleries"));
+		$route->register($this, "|^\/$|", array($this, "addGallery"), "POST");
 		$route->register($this, "|^\/(\d*)$|", array($this, "getGallery"));
+		$route->register($this, "|^\/(\d*)$|", array($this, "updateGallery"), "POST");
+		$route->register($this, "|^\/(\d*)$|", array($this, "deleteGallery"), "DELETE");
 	}
 
 	//GALLERY//
 	
 	function addGallery($input){
-		
+		AuthLevelOr403($this, MODERATOR);
+	
+		if(!isset($input->Title) || $input->Title == "")
+			throw new InvalidInputDataException("Field Title is required");
+			
+		$sth = $this->db->prepare("insert into imagegalleries values(0,?,?);");
+		if($sth->execute(array($input->Title, $this->auth->GetUserId())) == 0)
+			throw new Exception("Database update failed when adding gallery");
+			
+		return $this->getGallery(null, $this->db->lastInsertId());
 	}
 	
 	function updateGallery($input, $galleryId){
+		AuthLevelOr403($this, MODERATOR);
 	
+		if(!isset($galleryId) || $galleryId == "")
+			throw new InvalidInputDataException("Argument galleryId is required");
+		
+		if(!isset($input->Title) || $input->Title == "")
+			throw new InvalidInputDataException("Field Title is required");
+
+		$sth = $this->db->prepare("update imagegalleries set Title=? where GalleryId=? limit 1");
+		if($sth->execute(array($input->Title, $galleryId)) == 0)
+			throw new Exception("Database update failed when updating gallery");
+			
+		return $this->getGallery(null, $galleryId);
 	}
 	
 	function listGalleries(){
@@ -40,21 +64,40 @@ class gallery extends Module {
 		return $retval;
 	}
 	
+	function deleteGallery($input, $galleryId){
+		AuthLevelOr403($this, MODERATOR);
+		
+		if(!isset($galleryId) || $galleryId == "")
+			throw new InvalidInputDataException("Argument galleryId is required");
+			
+		$sth1 = $this->db->prepare("delete from imagelinking where GalleryId=?;");
+		$sth2 = $this->db->prepare("delete from imagegalleries where GalleryId=? limit 1;");
+		
+		$sth1->execute(array($galleryId));
+		$sth2->execute(array($galleryId));
+		
+		return true;
+	}
+	
 	//IMAGES//
 	
 	function uploadImage($input){
+		AuthLevelOr403($this, MODERATOR);
 	
 	}
 	
 	function removeImage($input, $imageId){
+		AuthLevelOr403($this, MODERATOR);
 	
 	}
 	
 	function linkImage($input, $imageId, $galleryId){
+		AuthLevelOr403($this, MODERATOR);
 	
 	}
 	
 	function unlinkImage($input, $imageId, $galleryId){
+		AuthLevelOr403($this, MODERATOR);
 	
 	}
 } 
