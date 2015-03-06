0129a7659b190c194a96d89a9769002716c6d85c
Niklas Hedlund <nojan1989@gmail.com>
2015-03-06 09:13:39 +0100

    Refined exception handling and added logging

--- src/exceptions.php
+++ src/exceptions.php
@@ -18,3 +18,17 @@ class ModuleHasNoSetupException extends Exception {}
 class InvalidInputDataException extends Exception {}
 
 class NoSuchResourceException extends Exception {}
+
+class NonInternalException extends Exception {}
+
+function LogException($db, $ex){
+	$rawInput = file_get_contents("php://input");
+	
+	$sth = $db->prepare("insert into exceptions (exceptionType, exceptionMessage, inputData) values(?,?,?)");
+	$sth->execute(array(get_class($ex), $ex->getMessage(), $rawInput));
+}
+
+function OutputFormatException($ex, $messageOverride = ""){
+	return json_encode(array("type" => get_class($ex),
+							 "message" =>  (($messageOverride == "") ? $ex->getMessage() : $messageOverride)));
+}
