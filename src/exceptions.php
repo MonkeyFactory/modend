a8cd93f2e7cb67e21dc19842276faa788ad66403
Niklas Hedlund <nojan1989@gmail.com>
2015-03-06 09:24:00 +0100

    Refactored exception helper methods to own class

--- src/exceptions.php
+++ src/exceptions.php
@@ -21,14 +21,16 @@ class NoSuchResourceException extends Exception {}
 
 class NonInternalException extends Exception {}
 
-function LogException($db, $ex){
-	$rawInput = file_get_contents("php://input");
-	
-	$sth = $db->prepare("insert into exceptions (exceptionType, exceptionMessage, inputData) values(?,?,?)");
-	$sth->execute(array(get_class($ex), $ex->getMessage(), $rawInput));
-}
+class ExceptionHelper {
+	function LogException($db, $ex){
+		$rawInput = file_get_contents("php://input");
+		
+		$sth = $db->prepare("insert into exceptions (exceptionType, exceptionMessage, inputData) values(?,?,?)");
+		$sth->execute(array(get_class($ex), $ex->getMessage(), $rawInput));
+	}
 
-function OutputFormatException($ex, $messageOverride = ""){
-	return json_encode(array("type" => get_class($ex),
-							 "message" =>  (($messageOverride == "") ? $ex->getMessage() : $messageOverride)));
+	function OutputFormatException($ex, $messageOverride = ""){
+		return json_encode(array("type" => get_class($ex),
+								 "message" =>  (($messageOverride == "") ? $ex->getMessage() : $messageOverride)));
+	}
 }
